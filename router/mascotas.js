a40951ba4fa056ee44c6e3a6a1f32a112adae769
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-10-05 20:38:43 -0500

    Agregue metodo Get para ir a un registro en especifico (Update)

--- router/mascotas.js
+++ router/mascotas.js
@@ -48,6 +48,40 @@ router.post('/', async(req,res)=>{
     console.log(error)
   }
 
+})
+
+
+// ocupamos async y await por que se estan haciendo solicitudes a las DB
+// Para hacer el update del CRUD necesitamos el id por lo tanto
+// necesitamos una variable dinamica para la url. En este caso la obtenemos con /:id 
+router.get('/:id', async (req, res) => {
+
+  // creando la variable id
+  const id = req.params.id
+
+  try {
+    
+    // buscando el id del registro a editar en la DB
+    // En mongo DB los id esta con un guion bajo "_" por eso al pasar el paremetro id al
+    // metodo findOne debemos pasarle un objeto donde igualemos el id al _id
+    const mascotaDB = await Mascota.findOne({_id:id})
+
+    // enviar la edicion a una vista
+    // Si todo sale bien se va a pintar los datos de la mascota y el error por tanto esta en false
+    res.render('editar',{
+      mascota:mascotaDB,
+      error:false
+    })
+
+  } catch (error) {
+
+    // Si la busqueda fallo y el id no existe entonces el error es true y no hay datos que mostar
+    // Pero si podemos mandar un mensaje
+    res.render('editar',{
+      error:true,
+      mensaje: 'No se encontro el id seleccionado'
+    })
+  }
 
 })
 
